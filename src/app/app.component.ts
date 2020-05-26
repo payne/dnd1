5b5917d047873767bfa7d1f9f0d0525e6e9d91c9
Matt Payne <Payne@MattPayne.org>
2020-05-25 22:14:14 -0500

    wip; added bit about future work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {MatTabGroup} from '@angular/material/tabs';
+import {TabType} from './tab';
+
 
 @Component({
   selector: 'app-root',
@@ -8,8 +12,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'dnd1';
   movies = ['Heat', 'Xanadu', 'Reds', 'Harold & Maude'];
+  tabs = [ new TabType('tab 1', 'I like eggs'),
+    new TabType('tab 2', 'It is late at night'),
+    new TabType('tab 3', 'Third time is kinda a charm')
+  ];
 
-  drop(event) {
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+
+  dropped(event: CdkDragDrop<any>, t: MatTabGroup): void {
+    const arr = t._tabs.toArray();
+    moveItemInArray(arr, event.previousIndex, event.currentIndex);
+    t._tabs.reset(arr);
+  }
+
+  dropTab(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.tabs, event.previousIndex, event.currentIndex);
+  }
+
+  dropOld(event) {
     console.log(`drop method`);
     console.log(event);
   }
---
+++ src/app/tab.ts
@@ -0,0 +1,8 @@
+export class TabType {
+  content: string;
+  label: string;
+  constructor(label: string, content: string) {
+    this.label = label;
+    this.content = content;
+  }
+};
