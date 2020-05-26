b4264f43b2af39a6cef44fcf267bf0d6283813ad
Matt Payne <Payne@MattPayne.org>
2020-05-25 21:07:06 -0500

    ng add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, DragDropModule
+    AppRoutingModule, DragDropModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
